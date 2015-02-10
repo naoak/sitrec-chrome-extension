a5e47f2473d6719da7f9ac1303ac9ed4cd8c0406
Naoaki Yamada <n-yamada@s-cradle.com>
2015-02-11 05:42:42 +0900

    Support graceful stop

--- screenshot.js
+++ screenshot.js
@@ -1,17 +1,21 @@
 var DEFAULT_FPS = 10;
 var QUALITY = 50;
-var RESOLVE_DELAY = 2000;
+var RESOLVE_DELAY = 500;
 var fps;
+var album;
 
 var isRecording = false;
 var timer = null;
 var images = [];
 var startDate;
+var onResolveLoadListener = null;
+var onLoadListener = null;
 
 function IntervalTimer(proc, span) {
   this.proc = proc;
   this.span = span;
   this.timer = null;
+  this.gracefullStopCallback = null;
 }
 
 IntervalTimer.prototype.start = function() {
@@ -24,15 +28,27 @@ IntervalTimer.prototype.start = function() {
     var diff;
     time += span;
     proc();
-    diff = Date.now() - start - time;
-    self.timer = setTimeout(instance, (span - diff));
+    if (self.gracefullStopCallback) {
+      self.gracefullStopCallback();
+      self.gracefullStopCallback = null;
+      self.timer = null;
+    }
+    else {
+      diff = Date.now() - start - time;
+      self.timer = setTimeout(instance, (span - diff));
+    }
   }
   self.timer = setTimeout(instance, span);
 }
 
-IntervalTimer.prototype.stop = function() {
-  clearTimeout(this.timer);
-  this.timer = null;
+IntervalTimer.prototype.stop = function(callback) {
+  if (callback) {
+    this.gracefullStopCallback = callback;
+  }
+  else {
+    clearTimeout(this.timer);
+    this.timer = null;
+  }
 }
 
 IntervalTimer.prototype.isActive = function() {
@@ -46,28 +62,48 @@ IntervalTimer.prototype.setProcedure = function(proc) {
 function startRecording(options) {
   var i = 0;
   fps = parseInt((options.fps || DEFAULT_FPS), 10);
+  album = options.album || '';
   chrome.browserAction.setIcon({path: 'images/icon-rec.png'});
   chrome.browserAction.setTitle({title: 'Stop recording.'});
   images = [];
-  if (options.resolveUrl) {
-    chrome.tabs.update({
-      url: options.resolveUrl
-    });
-  }
-  setTimeout(function() {
+
+  function load() {
+    onLoadListener = function(details) {
+      if (details.url.split('#')[0].indexOf(options.url.split('#')[0]) == 0) {
+        stopRecording();
+      }
+    };
+    chrome.webNavigation.onCompleted.addListener(onLoadListener);
     startDate = Date.now();
     chrome.tabs.update({
       url: options.url
     });
-    takePhoto(i, images, options);
+    takePhoto(i, images);
     timer = new IntervalTimer(function() {
-      takePhoto(++i, images, options);
+      takePhoto(++i, images);
     }, 1000 / fps);
     timer.start();
-  }, options.resolveUrl ? RESOLVE_DELAY : 0);
+  }
+  if (options.resolveUrl) {
+    onResolveLoadListener = function(details) {
+      if (options.resolveUrl.split('#')[0] == details.url.split('#')[0]) {
+        chrome.webNavigation.onCompleted.removeListener(onResolveLoadListener);
+        setTimeout(function() {
+          load();
+        }, RESOLVE_DELAY);
+      }
+    }
+    chrome.webNavigation.onCompleted.addListener(onResolveLoadListener);
+    chrome.tabs.update({
+      url: options.resolveUrl
+    });
+  }
+  else {
+    load();
+  }
 }
 
-function takePhoto(i, images, options) {
+function takePhoto(i, images, callback) {
   chrome.browserAction.setBadgeText({text: '' + i});
   chrome.tabs.captureVisibleTab(null, {quality: QUALITY}, function(img) {
     img = img ? img : 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
@@ -76,14 +112,24 @@ function takePhoto(i, images, options) {
       time: Date.now() - startDate,
       data: img
     });
+    if (callback) {
+      callback();
+    }
   });
 }
 
 function stopRecording() {
-  chrome.browserAction.setIcon({path: 'images/icon.png'});
-  chrome.browserAction.setTitle({title: 'Start recording.'});
+  function doStop() {
+    if (onLoadListener) {
+      chrome.webNavigation.onCompleted.removeListener(onLoadListener);
+      onLoadListener = null;
+    }
+    chrome.browserAction.setIcon({path: 'images/icon.png'});
+    chrome.browserAction.setTitle({title: 'Start recording.'});
+    showVideoPlaybackPage();
+  }
   timer.stop();
-  showVideoPlaybackPage();
+  doStop();
 }
 
 function showVideoPlaybackPage() {
@@ -95,12 +141,13 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.toggleRecord) {
     if (isRecording) {
       stopRecording();
-    } else {
+    }
+    else {
       startRecording(request.toggleRecord);
     }
     isRecording = !isRecording;
+    sendResponse({isRecording: isRecording});
   }
-  sendResponse({isRecording: isRecording});
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
