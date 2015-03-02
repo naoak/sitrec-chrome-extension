1419a434f5c8ca0c8d48e2a479b902e201785af2
Naoaki Yamada <n-yamada@s-cradle.com>
2015-03-02 13:45:12 +0900

    Fix defaults and placeholders

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 document.addEventListener('DOMContentLoaded', function() {
   var inputInfos = [
     {name: 'album', default: ''},
-    {name: 'server', default: 'localhost:11080'},
+    {name: 'server', default: 'http://localhost:11080'},
     {name: 'httpProxy', default: 'localhost:11001'},
     {name: 'initialUrl', default: 'http://www.example.com/empty'},
     {name: 'url', default: 'http://www.example.com/'},
